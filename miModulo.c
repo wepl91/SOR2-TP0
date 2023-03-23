b768cebb77b7d97ec7acc33eef7832607f9b2e3e
Walter Pereyra <w.e.p.l.91@gmail.com>
2023-03-23 15:04:58 -0300

    Init module

--- miModulo.c
+++ miModulo.c
@@ -1,18 +1,21 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/uaccess.h>
 
 int init_module(void);
 void cleanup_module(void);
 
 #define SUCCESS 0
 #define DEVICE_NAME "charDevice"
+#define BUF_LEN 80
 
 /*
  * Global variables
  */
-
+static int Major;
 static struct file_operations fops = {
+    .owner = THIS_MODULE;
     .read = device_read,
     .write = device_write,
     .open = device_open,
@@ -21,8 +24,15 @@ static struct file_operations fops = {
 /* Constructor */
 int init_module(void)
 { 
-    printk(KERN_INFO "UNGS : Driver registrado\n");
-    return 0;
+    Major = register_chrdev(0, DEVICE_NAME, &fops);
+    
+    if (Major < 0)
+    {
+        printk(KERN_INFO "Registrando driver con %d\n", Major);
+        return Major
+    }
+    printk(KERN_INFO "Driver registrado en %d\n", Major);
+    return SUCCESS;
 }
 
 /* Destructor */
