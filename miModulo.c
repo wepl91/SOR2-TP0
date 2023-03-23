6bfe28a9c61e19ad4580716fbf5703902cd1b6f2
Walter Pereyra <w.e.p.l.91@gmail.com>
2023-03-23 14:59:04 -0300

    Life cicly methods of module

--- miModulo.c
+++ miModulo.c
@@ -1,15 +1,49 @@
-#include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
 
+int init_module(void);
+void cleanup_module(void);
+
+#define SUCCESS 0
+#define DEVICE_NAME "charDevice"
+
+/*
+ * Global variables
+ */
+
+static struct file_operations fops = {
+    .read = device_read,
+    .write = device_write,
+    .open = device_open,
+    .release = device_release};
+
+/* Constructor */
 int init_module(void)
-{ /* Constructor */
-  printk(KERN_INFO "UNGS : Driver registrado\n");
-  return 0;
+{ 
+    printk(KERN_INFO "UNGS : Driver registrado\n");
+    return 0;
 }
 
+/* Destructor */
 void cleanup_module(void)
-{ /* Destructor */
-  printk(KERN_INFO "UNGS : Driver desregistrado\n");
+{ 
+    printk(KERN_INFO "UNGS : Driver desregistrado\n");
+}
+
+static int device_open(struct inode *inode, struct file *filp)
+{
+    return SUCCESS;
+}
+
+static int device_release(struct inode *inode, struct file *filp)
+{
+    return SUCCESS;
+}
+
+static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t *offset)
+{
+    return length;
 }
 
 MODULE_LICENSE("GPL");
